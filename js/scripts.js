f8c076aada295ef197ea2622b45b923822a9b633
Thomas Glenn <thomasgrantglenn@mac.com>
2020-06-12 10:20:49 -0600

    add for loop and conditions ('don't yet know if it workds...)

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 // this is business logic
 userArray = [];
 for (i=0; i<=userNumber; i++);
-  
+  let robogerArray = userArray[i];
 
-if ( )
+if (robogerArray.includes(1)) {
+  robogerArray.replace("Beep!");
+} else if (robogerArray.includes(2)) {
+  robogerArray.replace("Boop!");
+} else if (robogerArray.includes(3)) {
+  robogerArray.replace("Won't you be my neighbour?");
+} else {
+  return robogerArray;
+}
 
-
+console.log();
 
 // this is user-interface logic
 $(document).ready(function() {

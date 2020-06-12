137ca4d4bdd0711e576df896552d5cd6ac4181c2
Thomas Glenn <thomasgrantglenn@mac.com>
2020-06-12 14:12:49 -0600

    place the array push AT THE END of the conditional statements (instead of the beginning...).

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,22 @@ function userArray(userNumber) {
   let array = [];
   for (index = 0; index <= userNumber; index++) {
     const words = index.toString();
-    
-    if (words.includes(1)) {
-      array.push(array.push("Beep!"));
+
+    if (words.includes(3)) {
+      array.push("Won't you be my neighbour?");
+    } else if (words.includes(2)) {
+      array.push("Boop!");
+    } else if (words.includes(1)) {
+      array.push("Beep!");
+    } else {
+      array.push(words);
     }
-  
+   
    }
+  
    return array;
 }
 
-console.log("This is the end of the array.");
 
 
 // this is user-interface logic
@@ -20,14 +26,11 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     let userName = $("#name").val();
-    let userNumber = parseInt($("#number").val());
+    let userNumber = ($("#number").val());
     let result = userArray(userNumber);
 
     $(".name").text(userName);
     $(".number").text(result);
-// 
-    
-// 
 
 
     $("#output").show();
